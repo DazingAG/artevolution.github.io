7edbaecc68904e94d83eff91262cec7068f16ef1
wynnee <jraschieka@gmail.com>
2025-11-27 00:20:15 +0800

    11/27/25 1220 : carousel

--- ArtEvo/script2.js
+++ ArtEvo/script2.js
@@ -1,10 +1,11 @@
     //Image carousel
    const carousel = document.getElementById("carouselImage");
    const images = [
+    "carousel/artevo.png",
     "carousel/concepcion.jpeg",
     "carousel/guevarra.png",
     "carousel/janda.png",
-    "carousel/santos.jpeg",
+    "carousel/santos.jpeg"
 ];
 
         let index = 0;
