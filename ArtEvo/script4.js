5130220e5d8202b0b9f15d8e94c4948c28accf7f
wynnee <jraschieka@gmail.com>
2025-11-27 04:54:51 +0800

    11/27/25 0454 : contact

--- ArtEvo/script4.js
+++ ArtEvo/script4.js
@@ -1,10 +1,18 @@
-const carouselImages = ["contact1.jpg", "contact2.jpg", "contact3.jpg"];
+const carousel = document.getElementById("carouselImage");
+   const images = [
+    "carousel/artevo.png",
+    "carousel/concepcion.jpeg",
+    "carousel/guevarra.png",
+    "carousel/janda.png",
+    "carousel/santos.jpeg"
+];
+
 let index = 0;
 
 setInterval(() => {
-    index = (index + 1) % carouselImages.length;
-    document.getElementById("carouselImage").src = carouselImages[index];
-}, 3000);
+            index = (index + 1) % images.length;
+            carousel.src = images[index];
+        }, 3000);
 
 document.getElementById("sendMessage").addEventListener("click", () => {
     alert("Thank you! Your message has been sent.");
